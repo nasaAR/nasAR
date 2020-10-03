fe33c78c72d1d14b199e83f18e9c86110069e5db
Ji Hye Park <jhp0621@gmail.com>
2020-10-03 15:07:59 -0500

    Initial Commit

---
+++ nasAR2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  nasAR2
+//
+//  Created by Ji Hye Park on 10/3/20.
+//  Copyright © 2020 DAJSpace. All rights reserved.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
